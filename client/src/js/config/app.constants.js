98086823071dae643042f58d444cf7294860dc3d
Michael F. Rieser <rieser.20@osu.edu>
2018-08-20 13:55:50 -0400

    changed AppConstants.api from localhost to Heroku API app

--- client/src/js/config/app.constants.js
+++ client/src/js/config/app.constants.js
@@ -1,6 +1,6 @@
 const AppConstants = {
-  // api: 'http://localhost:3000/api',
-  api: 'http://localhost:3000', 
+  // api: 'http://localhost:3000', // LOCAL
+  api: 'https://todo-tomatoinator-api.herokuapp.com/',
   // api: '0.0.0.0:3000',
   // api: 'https://todo-tomatoinator-mrieser100.c9users.io',
   jwtKey: 'jwtToken',
