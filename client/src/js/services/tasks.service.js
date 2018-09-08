6bf0f1d09d316c8beb77f774bf8c5433ebc42e4b
Michael F. Rieser <rieser.20@osu.edu>
2018-09-08 12:36:48 -0400

    added logging for AppConstants svc

--- client/src/js/services/tasks.service.js
+++ client/src/js/services/tasks.service.js
@@ -6,6 +6,8 @@ export default class Tasks {
     this._PomTimer = PomTimer;
     this._$http = $http;
 
+    console.log('AppConstants.testKey: ', AppConstants.testKey);
+
     this.currentlySetFilters = {}; // TODO: hook this into local session storage
   }
 
