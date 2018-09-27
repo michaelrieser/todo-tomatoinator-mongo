82c0102d9a53c0329093944932d6799560aa6a8f
Michael F. Rieser <rieser.20@osu.edu>
2018-09-27 14:04:49 -0400

    added viewport meta tag and styling for mobile task-navbar

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -122,14 +122,12 @@ gulp.task('vendor_build_assets', function () {
 // CONDITIONALLY launch server => browser-sync (LOCAL) || gulp-connect (PRODUCTION) 
 gulp.task('default', ['html', 'gulp-config', 'browserify', 'sass', 'sass:watch', 'sounds', 'vendor_build_assets'], function () {
   if (process.env.NODE_ENV === 'production') {
-    log('PROD')
     connect.server({ // ** THIS IS DEFINITELY NOT THE RIGHT WAY TO WORK THIS - HAVING THIS gulp-connect server SERVE Express server??? **
       root: "./build",
       // livereload: true,
       port: process.env.PORT || 8080 // process.env.PORT provided by Heroku
     });
   } else {
-    log('!PROD')
     browserSync.init(['./build/**/**.**'], {
       server: "./build",
       // port: 4000,
