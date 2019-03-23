6718418819128e986420518dde2b19f5ea1d87a1
Michael F. Rieser <rieser.20@osu.edu>
2019-03-23 16:31:00 -0400

    UNFINISHED datetime picker updates - still need to show in input on load

--- client/src/js/components/date-time-picker.directive.js
+++ client/src/js/components/date-time-picker.directive.js
@@ -3,6 +3,9 @@ function dateTimePicker() {
     
     return {
         restrict: "A",
+        scope: {
+            onEnterKeypress: '&'
+        },
         require: "ngModel",
         link: function (scope, element, attrs, ngModelCtrl) {            
             // NOTE: Valid Moment Date instantiation (with format): moment('2018-02-09T05:00:00.000Z', 'YYYY/MM/DD, h:mm a')
@@ -11,17 +14,22 @@ function dateTimePicker() {
             var dtp = parent.datetimepicker({
                 keyBinds: {
                     enter: function() {
-                        console.log(scope)
-                        console.log(ngModelCtrl)
-                        // TODO: allow user to submit date with Enter keypress - need to call parent
-                        //  => could do something like scope.$ctrl.<method-to-blur-element/submit>, but NEED TO MAKE SURE METHOD SHARED B/TW TASK + NOTIFICATION DATE TIME PICKERS
+                        element.blur();
                     }
                 },
-                defaultDate: moment().format(attrs.format),
-                useCurrent: false, // sets date to null || existing dueDateTime when datetimepicker loaded, fixes datetimepicker vanishing when calendar clicked on task   
+                useCurrent: false, // sets date to null || existing dueDateTime when datetimepicker loaded, fixes datetimepicker vanishing when calendar clicked on task  
+                defaultDate: moment().add(1, 'hour').startOf('hour').format(attrs.format),                 
                 format: attrs.dateTimePicker,
                 showClear: true
-            });            
+            });         
+            
+            // added dp.show to update model when user elects suggested date time
+            // **TODO: still doesn't update input on load! => MAYBE THAT'S OK!?
+            dtp.on("dp.show", function (e) {
+                ngModelCtrl.$setViewValue(moment().add(1, 'hour').startOf('hour').toISOString())            
+                scope.$apply();                
+            });
+
             dtp.on("dp.change", function (e) {
                 if (e.date) {
                     ngModelCtrl.$setViewValue(e.date.toISOString()); // instantiating Moment object with non-ISO string deprecated and soon to be removed, so converting from Moment => ISO String
