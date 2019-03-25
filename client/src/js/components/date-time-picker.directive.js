6f2bdde81acbb238d752cc02b3de9c70126fea14
Rieser, Michael <RIESEM1@nationwide.com>
2019-03-25 08:08:59 -0400

    update input date when datetimepicker widget shown

--- client/src/js/components/date-time-picker.directive.js
+++ client/src/js/components/date-time-picker.directive.js
@@ -10,7 +10,7 @@ function dateTimePicker() {
         link: function (scope, element, attrs, ngModelCtrl) {            
             // NOTE: Valid Moment Date instantiation (with format): moment('2018-02-09T05:00:00.000Z', 'YYYY/MM/DD, h:mm a')
             //       Instantiating Moment Date with format 'MMMM Do YYYY, h:mm a' is not valid
-            var parent = $(element).parent();            
+            var parent = $(element).parent();                        
             var dtp = parent.datetimepicker({
                 keyBinds: {
                     enter: function() {
@@ -23,24 +23,26 @@ function dateTimePicker() {
                 showClear: true
             });         
             
-            // added dp.show to update model when user elects suggested date time
-            // **TODO: still doesn't update input on load! => MAYBE THAT'S OK!?
             dtp.on("dp.show", function (e) {
-                ngModelCtrl.$setViewValue(moment().add(1, 'hour').startOf('hour').toISOString())            
-                scope.$apply();                
+                scope.$apply( () => {
+                    ngModelCtrl.$setViewValue(moment().add(1, 'hour').startOf('hour').toISOString());                            
+                    ngModelCtrl.$modelValue = ''; // triggers $formatters to run - SEE: https://stackoverflow.com/questions/13995414/have-formatters-be-called-when-view-is-first-populated
+                });                                
             });
 
             dtp.on("dp.change", function (e) {
-                if (e.date) {
-                    ngModelCtrl.$setViewValue(e.date.toISOString()); // instantiating Moment object with non-ISO string deprecated and soon to be removed, so converting from Moment => ISO String
-                } else {
-                    ngModelCtrl.$setViewValue(null);   
-                }                
-                scope.$apply();
+                scope.$apply( () => {
+                    if (e.date) {
+                        ngModelCtrl.$setViewValue(e.date.toISOString()); // instantiating Moment object with non-ISO string deprecated and soon to be removed, so converting from Moment => ISO String
+                    } else {
+                        ngModelCtrl.$setViewValue(null);   
+                    }                                
+                    ngModelCtrl.$modelValue = '';
+                });
             });
 
             // Use formatter to update date display (without changing ngModel) - https://medium.com/made-by-munsters/build-a-text-date-input-with-ngmodel-parsers-and-formatters-5b1732e0ced4
-            let formatter = function formatter(value) {                
+            let formatter = function formatter(value) {   
                 return value ? moment(value).format(attrs.dateTimePicker) : value;
             }
             ngModelCtrl.$formatters.push(formatter);
--- client/src/js/components/task-helpers/task-due-date-time-input.component.js
+++ client/src/js/components/task-helpers/task-due-date-time-input.component.js
@@ -12,7 +12,7 @@ class TaskDueDateTimeInputCtrl {
         //  SEE: https://stackoverflow.com/questions/18626039/apply-already-in-progress-error (comment by Onur Yildrium)
         this._$interval( () => { this.task.dueDateTime = null; }); 
         
-        if (this.editmode) {
+        if (this.editmode) { // TODO: this could probably be deleted, since we don't need to update/remove due-date-time on new task
             this._Tasks.update(this.task).then(
                 (updatedTask) => {return updatedTask},
                 (err) => console.log(err)
@@ -24,7 +24,7 @@ class TaskDueDateTimeInputCtrl {
 
 let TaskDueDateTimeInput =  {
     bindings: {
-        displayingduedatetimeinput: '=',
+        displayingduedatetimeinput: '=', // TODO: convert attribute to kebab-case in view to allow for camelCase here
         task: '=',
     },
     controller: TaskDueDateTimeInputCtrl,
